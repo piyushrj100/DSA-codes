c84801fedb4ff8c73f2b04e2b66df46600fcf14c
Piyush Raja <piyush1.bksc@gmail.com>
2022-02-16 23:39:10 +0530

    Simple Hashing

---
+++ Hashing.py
@@ -0,0 +1,31 @@
+class Hashing :
+    def __init__(self) :
+        self.capacity=20
+        self.hash_table=[None]*self.capacity
+
+
+    def get_hash(self,key) :
+        hash=0
+        for char in key :
+            hash+=ord(char)
+        return hash%self.capacity
+     
+    def get(self,index) :
+        h=self.get_hash(index)
+        return self.hash_table[h]
+    
+    def add(self,key,val) :
+        h=self.get_hash(key)
+        self.hash_table[h]=val
+
+    def delete(self,key) :
+        h=self.get_hash(key)
+        self.hash_table[h]=None
+
+
+hash=Hashing()
+hash.add('hello there',76)
+hash.add('Hello World',5)
+res=hash.get('hello there')
+print(res)
+
